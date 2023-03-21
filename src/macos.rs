e8b068c1a39dbd4147d1f8ea65bd97d65f9e5d37
Francisco Jiménez Cabrera <jkfran@gmail.com>
2023-03-21 23:35:48 +0100

    Add Macos support and UDP ports

---
+++ src/macos.rs
@@ -0,0 +1,109 @@
+use libproc::libproc::file_info::pidfdinfo;
+use libproc::libproc::file_info::{ListFDs, ProcFDType};
+use libproc::libproc::net_info::{SocketFDInfo, SocketInfoKind};
+use libproc::libproc::proc_pid::{listpidinfo, pidinfo};
+use libproc::libproc::task_info::TaskAllInfo;
+use libproc::processes::{pids_by_type, ProcFilter};
+use log::{debug, info, warn};
+use nix::sys::signal::{self, Signal};
+use nix::unistd::Pid;
+use std::ffi::CStr;
+use std::io;
+
+/// Collect information about all processes.
+///
+/// # Returns
+///
+/// A vector containing the information of all processes.
+fn collect_proc() -> Vec<TaskAllInfo> {
+    let mut base_procs = Vec::new();
+
+    if let Ok(procs) = pids_by_type(ProcFilter::All) {
+        for p in procs {
+            if let Ok(task) = pidinfo::<TaskAllInfo>(p as i32, 0) {
+                base_procs.push(task);
+            }
+        }
+    }
+
+    base_procs
+}
+
+/// Kill processes listening on the specified port.
+///
+/// # Arguments
+///
+/// * `port` - The port number to kill processes listening on.
+///
+/// # Returns
+///
+/// A `Result` containing a boolean value. If true, at least one process was killed; otherwise, false.
+pub fn kill_processes_by_port(port: u16) -> Result<bool, io::Error> {
+    let process_infos = collect_proc();
+    let mut killed = false;
+
+    for task in process_infos {
+        let pid = task.pbsd.pbi_pid as i32;
+        let mut kill_process = false;
+
+        let fds = listpidinfo::<ListFDs>(pid, task.pbsd.pbi_nfiles as usize);
+        if let Ok(fds) = fds {
+            for fd in fds {
+                if let ProcFDType::Socket = fd.proc_fdtype.into() {
+                    if let Ok(socket) = pidfdinfo::<SocketFDInfo>(pid, fd.proc_fd) {
+                        match socket.psi.soi_kind.into() {
+                            SocketInfoKind::In => {
+                                if socket.psi.soi_protocol == libc::IPPROTO_UDP {
+                                    let info = unsafe { socket.psi.soi_proto.pri_in };
+                                    let local_port = u16::from_be(info.insi_lport as u16);
+                                    if local_port == port {
+                                        kill_process = true;
+                                        break;
+                                    }
+                                }
+                            }
+                            SocketInfoKind::Tcp => {
+                                let info = unsafe { socket.psi.soi_proto.pri_tcp };
+                                let local_port = u16::from_be(info.tcpsi_ini.insi_lport as u16);
+                                if local_port == port {
+                                    kill_process = true;
+                                    break;
+                                }
+                            }
+                            _ => (),
+                        }
+                    }
+                }
+            }
+        }
+
+        if kill_process {
+            debug!("Found process with PID {}", pid);
+            let pid = Pid::from_raw(pid);
+            let cmd = unsafe {
+                CStr::from_ptr(task.pbsd.pbi_comm.as_ptr())
+                    .to_string_lossy()
+                    .into_owned()
+            };
+
+            if cmd.starts_with("com.docker") {
+                warn!("Warning: Found Docker. You might need to stop the container manually.");
+            } else {
+                info!("Killing process with PID {}", pid);
+                match signal::kill(pid, Signal::SIGKILL) {
+                    Ok(_) => {
+                        killed = true;
+                    }
+                    Err(e) => {
+                        return Err(io::Error::new(
+                            io::ErrorKind::Other,
+                            format!("Failed to kill process {}: {}", pid, e),
+                        ));
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(killed)
+}
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,19 @@
 //! The utility accepts a list of port numbers as input and attempts to
 //! terminate any processes listening on those ports.
 
-mod port;
-mod process;
+#[cfg(target_os = "linux")]
+mod linux;
+#[cfg(target_os = "macos")]
+mod macos;
+
+#[cfg(target_os = "linux")]
+use linux::kill_processes_by_port;
+#[cfg(target_os = "macos")]
+use macos::kill_processes_by_port;
 
 use clap::Parser;
 use clap_verbosity_flag::{Verbosity, WarnLevel};
 use log::error;
-use port::kill_port;
 use std::process::exit;
 
 /// The `KillPortArgs` struct is used to parse command-line arguments for the
@@ -19,7 +25,11 @@ use std::process::exit;
 #[command(author, version, about, long_about = None)]
 struct KillPortArgs {
     /// A list of port numbers to kill processes on.
-    #[arg(name = "ports", help = "The list of port numbers to kill processes on")]
+    #[arg(
+        name = "ports",
+        help = "The list of port numbers to kill processes on",
+        required = true
+    )]
     ports: Vec<u16>,
 
     /// A verbosity flag to control the level of logging output.
@@ -51,7 +61,7 @@ fn main() {
 
     // Attempt to kill processes listening on specified ports
     for port in args.ports {
-        match kill_port(port) {
+        match kill_processes_by_port(port) {
             Ok(killed) => {
                 if killed {
                     println!("Successfully killed process listening on port {}", port);
